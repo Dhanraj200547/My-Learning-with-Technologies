a127d204e2fc9fbede84610eb2a9e705c5c2aa96
Dhanraj200547 <164746257+Dhanraj200547@users.noreply.github.com>
2025-07-30 14:39:05 +0530

    Bruteforce method updated

--- Python_Learning/StriversDSA/Arrays/Easy/LongestSubarray.py
+++ Python_Learning/StriversDSA/Arrays/Easy/LongestSubarray.py
@@ -1,7 +1,16 @@
+
+# Bruteforce Approach: 
+
 def longestSubarray(nums, k):
-    maxLen = 0
+    maxlen = 0
     for i in range(len(nums)):
         sum = 0
         for j in range(i,len(nums)):
-            sum += j
-            
+            sum += nums[j]
+            if sum == k:
+                maxlen = max(maxlen , j -i + 1)
+    return maxlen
+    
+nums =  [10, 5, 2, 7, 1, 9]
+k = 15
+print(longestSubarray(nums,k))
